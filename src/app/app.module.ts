844632da6b0a653979f6bb426f34c7fd503619b7
Golla <mjg1021@sru.edu>
2019-12-02 12:50:29 -0500

    added the api calls for each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { PlanetsComponent } from './planets/planets.component';
 import { StarshipsComponent } from './starships/starships.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/starwars.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarwarsService {
+
+  constructor(private http: HttpClient) { }
+
+  getPeoples() {
+    return this.http.get('https://swapi.co/api/people/  ');
+  }
+
+  getPeople(id) {
+    return this.http.get(`https://swapi.co/api/people/${id}`);
+  }
+
+  getPlanets() {
+    return this.http.get('https://swapi.co/api/planets/  ');
+  }
+
+  getPlanet(id) {
+    return this.http.get(`https://swapi.co/api/planets/${id}`);
+  }
+  getStarships() {
+    return this.http.get('https://swapi.co/api/starships/  ');
+  }
+
+  getStarship(id) {
+    return this.http.get(`https://swapi.co/api/starships/${id}`);
+  }
+}
